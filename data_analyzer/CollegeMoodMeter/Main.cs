e2f0938ade9d6966be9876519b0138d7efca8dd4
Kevin Payravi <kevinpayravi@gmail.com>
2014-10-05 07:42:52 -0400

    Final additions to C# program

--- data_analyzer/CollegeMoodMeter/Main.cs
+++ data_analyzer/CollegeMoodMeter/Main.cs
@@ -49,7 +49,8 @@ namespace CollegeMoodMeter
             int[] topWordsCountsOSU = topWordsCounts(wordDictionaryOSU);
             string[] spiritOSU = { "bestcollege", "coolestcollege", "football", "gameday", "gored", "undefeated", "brutus", "buckeye", "buckeyes", "buckeyebrutus", "gobucks", "goosu", "iloveosu", "loveosu", "osufootball", "scarletandgray", "scarletandgrey" };
             int[] scoresOSU = scorer("OSU", wordDictionaryOSU, happiness, sadness, stress, spiritOSU);
-            outputSchoolStats("Ohio State University", scoresOSU[0], scoresOSU[1], scoresOSU[2], scoresOSU[3], topWordsOSU, topWordsCountsOSU);
+            int totalScoreOSU = scoresOSU[0] - scoresOSU[1] - scoresOSU[2] + scoresOSU[3];
+            outputSchoolStats("Ohio State University", scoresOSU[0], scoresOSU[1], scoresOSU[2], scoresOSU[3], totalScoreOSU, topWordsOSU, topWordsCountsOSU);
 
             // Process Michigan data:
             Dictionary<string, int> wordDictionaryUM = new Dictionary<string, int>();
@@ -58,7 +59,8 @@ namespace CollegeMoodMeter
             int[] topWordsCountsUM = topWordsCounts(wordDictionaryUM);
             string[] spiritUM = { "bestcollege", "coolestcollege", "football", "gameday", "gored", "undefeated", "biff", "chadtough", "gobigblue", "goblue", "gomichigan", "wolverine", "wolverines" };
             int[] scoresUM = scorer("UM", wordDictionaryUM, happiness, sadness, stress, spiritUM);
-            outputSchoolStats("University of Michigan", scoresUM[0], scoresUM[1], scoresUM[2], scoresUM[3], topWordsUM, topWordsCountsUM);
+            int totalScoreUM = scoresUM[0] - scoresUM[1] - scoresUM[2] + scoresUM[3];
+            outputSchoolStats("University of Michigan", scoresUM[0], scoresUM[1], scoresUM[2], scoresUM[3], totalScoreUM, topWordsUM, topWordsCountsUM);
 
             //-------------------------------------------------------------------------------------------//
             // ADDING/CHANGING SCHOOLS? The only code that needs to be edited is above.
@@ -184,11 +186,13 @@ namespace CollegeMoodMeter
 
         private void outputHeader()
         {
-            string urlStyle = "https://raw.githubusercontent.com/OHIOhackathon2014/CollegeMoodMeter/master/css/style.css";
+            string urlStyle = "https://rawgit.com/OHIOhackathon2014/CollegeMoodMeter/master/css/style.css";
             string urlOSU = "https://raw.githubusercontent.com/OHIOhackathon2014/CollegeMoodMeter/master/images/osu.gif";
             string urlLogo = "https://raw.githubusercontent.com/OHIOhackathon2014/CollegeMoodMeter/master/images/logo.png";
             string urlMich = "https://raw.githubusercontent.com/OHIOhackathon2014/CollegeMoodMeter/master/images/mich.gif";
-            
+            string urlRepo = "https://github.com/OHIOhackathon2014/CollegeMoodMeter";
+            string urlOHIO = "https://www.ohiohackathon.org/";
+
             string[] outputCodeHeader = { "<!DOCTYPE html>",
                                        "<html lang=\"en\">",
                                        "<head>",
@@ -196,12 +200,12 @@ namespace CollegeMoodMeter
                                        "</head>",
                                        "<body>",
                                        "<div id = \"side\"> ",
-                                       "<div class = \"title\">College Mood Meter</div>",
                                        "<div class = \"description\">",
-                                       "Welcome to the College Mood Meter - a Twitter analysis program that identifies students from particular colleges and determines the schools' overall mood levels.",
+                                       "Welcome to the College Mood Meter - a Twitter analysis program that identifies students from particular colleges and determines the schools' overall mood levels.<br><br>Analysis is done through Twitter searches via the Twitter API. First, a list of students from each college is compiled by finding recent mentions of particular keywords and account mentions used most exclusively by current students from that particular college. The last 200 tweets for each of these users is then compiled. The most commonly occuring words are recorded, along with performing a search for specific keywords that indicate each of the four moods. Then, the number of occurences of these keywords are recorded. Using the extracted data, mood levels are assigned to each of the colleges based on the keyword occurrence counts.<br><br>The current program is built to analyze Ohio State and University of Michigan students, which can easily be expanded to other schools (or other user sets).",
                                        "</div>",
                                        "<br>",
-                                       "<a href = \"URL\"><div class = \"links\">GitHub Repo</div></a>",
+                                       "<a href = \"" + urlRepo + "\"><div class = \"links\">GitHub Repo</div></a>",
+                                       "<a href = \"" + urlOHIO + "\"><div class = \"links\">Made with <3 at OHI/O</div></a>",
                                        "</div>",
                                        "<div id = \"main\">",
                                        "<img src=\"" + urlOSU + "\" width=\"200px\">",
@@ -213,22 +217,36 @@ namespace CollegeMoodMeter
             
         }
 
-        private void outputSchoolStats(string school, int happinessScore, int sadnessScore, int stressScore, int spiritScore, string[] topWords, int[] topWordsCounts)
+        private void outputSchoolStats(string school, int happinessScore, int sadnessScore, int stressScore, int spiritScore, int totalScore, string[] topWords, int[] topWordsCounts)
         {
             string[] outputCodeStats = { "<br><br><br>",
-                                       "<div class = \"title\">" + school + "</div>",
+                                       "<div class = \"title\">"};
+            
+            string urlSchoolLogo = "";
+            if(school.Equals("Ohio State University")){
+                urlSchoolLogo = "https://raw.githubusercontent.com/OHIOhackathon2014/CollegeMoodMeter/master/images/osulogoclose.png";
+            }else if(school.Equals("University of Michigan")){
+                urlSchoolLogo = "https://raw.githubusercontent.com/OHIOhackathon2014/CollegeMoodMeter/master/images/michlogoclose.png";
+            }
+            
+            string[] outputCodeStats2 = { "<img src=\"" + urlSchoolLogo + "\" width=\"400px\"></div>",
                                        "<font style=\"font-size: 20px;\">Scores</font>",
                                        "<br>",
-                                       "Happiness - " + happinessScore + "<br>",
-                                       "Sadness - " + sadnessScore + "<br>",
-                                       "Stress - " + stressScore + "<br>",
-                                       "School Spirit - " + spiritScore + "<br>",
+                                       "Happiness - <font color=\"green\">" + happinessScore + "</font><br>",
+                                       "Sadness - <font color=\"red\">" + sadnessScore + "</font><br>",
+                                       "Stress - <font color=\"red\">" + stressScore + "</font><br>",
+                                       "School Spirit - <font color=\"green\">" + spiritScore + "</font><br>",
                                        "<br>",
+                                       "<font style=\"font-size: 20px;\">Total Score</font>",
+                                       "<br>",
+                                       "<font style=\"font-size: 20px;\">" + totalScore + "</font>",
+                                       "<br><br>",
                                        "<font style=\"font-size: 20px;\">Top Keywords</font>",
                                        "<br>",
                                        topWords[0] + " <font size=\"2\">(" + topWordsCounts[0] + ")</font> • " +  topWords[1] + " <font size=\"2\">(" + topWordsCounts[1] + ")</font> • " +  topWords[2] + " <font size=\"2\">(" + topWordsCounts[2] + ")</font> • " +  topWords[3] + " <font size=\"2\">(" + topWordsCounts[3] + ")</font><br>" +  topWords[4] + " <font size=\"2\">(" + topWordsCounts[4] + ")</font> • " +  topWords[5] + " <font size=\"2\">(" + topWordsCounts[5] + ")</font> • " +  topWords[6] + " <font size=\"2\">(" + topWordsCounts[6] + ")</font> • " +  topWords[7] + " <font size=\"2\">(" + topWordsCounts[7] + ")</font> • " +  topWords[8] + " <font size=\"2\">(" + topWordsCounts[8] + ")</font> • " +  topWords[9] + " <font size=\"2\">(" + topWordsCounts[9] + ")</font>"};
 
             File.AppendAllLines(path, outputCodeStats, Encoding.UTF8);
+            File.AppendAllLines(path, outputCodeStats2, Encoding.UTF8);
         }
 
         private void outputFooter()
